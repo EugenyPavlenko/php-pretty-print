748549c9dd381ffaa0d96e036eb25a63976fdef1
Eugeny Pavlenko <tangor@cagoi.com>
2013-08-25 17:11:25 +0300

    1) collapse long strings feature 2) collapse keys feature 3) better object printing 4) new methods: one & dump (accepting multiple variables), method "do_" is dropped 5) some optimizations and fixes

--- PP.php
+++ PP.php
@@ -9,20 +9,37 @@
 	 * License: MIT
 	 */
 	class PP {
-
-		// some settings
+		// some settings, you can easily change
 		private
 			//$printTime = true,
 			//$printBackTraces = true,
-			$doAlignKeys = true;
-		
-		private 
-			$ulMarginLeft = 40;
+			$doAlignKeys                    = true,
+			$doCollapseLongStrings          = true,
+			// if string's length exceeds that number, the string is then considered "long"
+			$longStringsLength              = 100,
+			$doCollapseKeys                 = true,
+			$keysCountToLeaveWhenCollapsing = 5,
+			// do apply key collapsing only if array contains certain number of keys
+			$applyKeyCollapseKeysLimit      = 10;
+			
+		private
+			// to be unique in a DOM
+			$libPrefix          = 'phpPrettyPrint_',
+			$ulMarginLeft       = 20,
+			$ulMarginLeftLevels = 20,
+			$tablesStyle        = 'border-collapse: collapse;',
+			$collapseSpanStyle  = 'border: 1px solid #dc143c; color: #dc143c; cursor: pointer; font-size: 11px; padding: 0px;',
+			$collapseSpanText   = '...',
+			$manySeparator      = "<br><br>";
 		
 		private function isAssocArr($arr)	{
 			return (bool)count(array_filter(array_keys($arr), 'is_string'));
 		}
 
+		private function genUniqueStr() {
+			return uniqid($this->libPrefix) . str_replace(".", "_", strval(microtime(true)));
+		}
+		
 		private function printKey($k, $keysMaxLen, $assoc) {
 			// 2 means two quotes (")
 			$plus = $assoc ? 2 : 0;
@@ -52,7 +69,32 @@
 		private function printValue($v) {
 			if (is_string($v)) {
 				$v = htmlentities($v);
-				return "<span style='color: #8b0000'>\"{$v}\"</span>";
+				
+				if ($this->doCollapseLongStrings && (mb_strlen($v) > $this->longStringsLength)) {
+					$tmpStr = '';
+					$tmpArr = explode(' ', $v);
+					$i      = 0;
+					
+					while (mb_strlen($tmpStr) < $this->longStringsLength) {
+						$tmpStr .= $tmpArr[$i] . ' ';
+						$i++;
+					}
+					
+					$spanTmpId      = $this->genUniqueStr()."_spanTmp";
+					$spanTmpIdMore  = $this->genUniqueStr()."_spanTmpMore";
+					$spanFullId     = $this->genUniqueStr()."_spanFull";
+					
+					$spanTmp = "<span id='{$spanTmpId}' style='color: #8b0000'>\"{$tmpStr}";
+					$spanTmp .= "<span id='{$spanTmpIdMore}' onclick='document.getElementById(\"{$spanTmpIdMore}\").style.display=\"none\";document.getElementById(\"{$spanTmpId}\").style.display=\"none\";document.getElementById(\"{$spanFullId}\").style.display=\"inline\";' style='{$this->collapseSpanStyle}'>{$this->collapseSpanText}</span>";
+					$spanTmp .= "\"</span>";
+					
+					//$v = $tmpStr;
+					$spanFull = "<span id='{$spanFullId}' style='display: none; color: #8b0000'>\"{$v}\"</span>";
+					
+					return $spanFull . $spanTmp;
+				} else {
+					return "<span style='color: #8b0000'>\"{$v}\"</span>";	
+				}
 			} elseif (is_array($v)) {
 				$v = "[]";
 				return "<span style='font-weight: bold'>{$v}</span>";
@@ -64,76 +106,123 @@
 				return "<span style='color: #00008b; font-weight: bold'>{$v}</span>";
 			} elseif (is_object($v)) {
 				$v = trim(print_r($v, true));
-				return "<span style='color: #000'>{$v}</span>";
+				return "<span style='color: #000'><pre style='font-size: 13px; padding: 0px; margin: 0px; display: inline'>{$v}</pre></span>";
 			} else {
 				$v = print_r($v, true);
 				return "<span style='color: #00008b'>{$v}</span>";
 			}
 		}
 		
-		private function doRecursion($arr, $firstCall) {
+		private function getArrayMaxKeyLength($arr) {
+			$keyLen = 0;
+			foreach ($arr as $key => $val) {
+				$t = (string)$key;
+				if (strlen($t) > $keyLen)
+					$keyLen = strlen($t);
+			}
+			
+			return $keyLen;
+		}
+		
+		private function doRecursion($someVar, $firstCall, $level= 0, $uniqueId) {
+			$isArray      = is_array($someVar);
+			$isAssocArray = $isArray ? $this->isAssocArr($someVar) : false;
+			$keysCount    = $isArray ? count($someVar) : 0;
+			$keyLen       = $isArray ? $this->getArrayMaxKeyLength($someVar) : 0;
 			
 			if ($firstCall) {
-				$mrg = is_array($arr) ? '0 0 0 '.$this->ulMarginLeft.'px' : '0';
-				$retStr = (is_array($arr) ? ($this->isAssocArr($arr) ? '{' : '[') : '') . '<ul style="list-style-type: none; padding: 0px; margin: '.$mrg.'">';
+				$mrg     = ($isArray && $keysCount) ? '0 0 0 '.$this->ulMarginLeft.'px' : '0';
+				
+				$retStr  = ($isArray && $keysCount ? ($isAssocArray ? '{' : '[') : '');
+				$retStr .= '<ul style="list-style-type: none; padding: 0px; margin: '.$mrg.'">';
 			} else {
-				$retStr = '<ul style="list-style-type: none; padding: 0px; margin-left: '.$this->ulMarginLeft.'px;">';
+				$retStr  = '<ul style="list-style-type: none; padding: 0px; margin-left: '.$this->ulMarginLeftLevels.'px;">';
 			}
 
-			if (is_array($arr) && (count($arr) > 0)) {
+			
+			if ($isArray && ($keysCount > 0)) {
+				
 				$i = 0;
-				$len = count($arr);
 
-				$keyLen = 0;
-				foreach ($arr as $key => $val) {
-					$t = (string)$key;
-					if (strlen($t) > $keyLen)
-						$keyLen = strlen($t);
-				}
+				foreach ($someVar as $key => $val) {
+					
+					$comma        = ($i == $keysCount-1) ? '' : ',';
+					$bracketOpen  = (is_array($val) && count($val)) ? ($this->isAssocArr($val) ? '{' : '[') : '';
+					$bracketClose = ($bracketOpen == '') ? '' : (($bracketOpen == '{') ? '}' : ']');
+					$pKey         = $this->printKey($key, $keyLen, $isAssocArray);
+					$pVal         = (is_array($val) && count($val)) ? $this->doRecursion($val, false, $level+1, $uniqueId) :  $this->printValue($val);
+					$liVisibility = ($this->doCollapseKeys && ($keysCount >= $this->applyKeyCollapseKeysLimit) && (($i+1) > $this->keysCountToLeaveWhenCollapsing)) ? 'none' : 'inline';
+					
+					$retStr      .= "<li style='display: {$liVisibility}' class='{$this->libPrefix}le{$level}'><table style='{$this->tablesStyle}'><tr valign='top'>";
+					$retStr      .= "<td>{$pKey}: </td>";
+					$retStr      .= "<td>{$bracketOpen}{$pVal}{$bracketClose}{$comma}</td>";
+					$retStr      .= "</tr></table></li>";
 
-				$assoc = $this->isAssocArr($arr);
-
-				foreach ($arr as $key => $val){
-					$comma = ($i == $len-1) ? '' : ',';
-
-					if (is_array($val) && (count($val) > 0)) {
-						if ($this->isAssocArr($val)) {
-							$retStr .= '<li>' . $this->printKey($key, $keyLen, $assoc) . ': {' . $this->doRecursion($val, false) . '}'.$comma.'</li>';
-						} else {
-							$retStr .= '<li>' . $this->printKey($key, $keyLen, $assoc) . ': [' . $this->doRecursion($val, false) . ']'.$comma.'</li>';
-						}
-					} else {
-						$retStr .= '<li>' . $this->printKey($key, $keyLen, $assoc) . ': ' . $this->printValue($val)  . $comma . '</li>';
+					if ($this->doCollapseKeys && ($keysCount >= $this->applyKeyCollapseKeysLimit) && (($i+1) == $this->keysCountToLeaveWhenCollapsing)) {
+						$spanId       = $this->genUniqueStr().'_span'; 
+						$retStr      .= "<span id='{$spanId}' onclick='{$uniqueId}_showCollapsedKeys({$level}); (function(x){x.parentNode.removeChild(x);})(document.getElementById(\"{$spanId}\"))' style='{$this->collapseSpanStyle}'>{$this->collapseSpanText}</span>";	
 					}
-
+					
 					$i++;
 				}
+				
 			} else {
-				$retStr .= '<li>' . $this->printValue($arr) . '</li>';
+				$retStr .= '<li>' . $this->printValue($someVar) . '</li>';
 			}
 
 			$retStr .= '</ul>';
 			
-			if ($firstCall && is_array($arr)) {
-				$retStr .= ($this->isAssocArr($arr) ? '}' : ']');
+			if ($firstCall && $isArray && $keysCount) {
+				$retStr .= $isAssocArray ? '}' : ']';
 			}
 
 			return $retStr;
 		}
 		
-		private function doPrettyPrint($arr) {
-			$r = $this->doRecursion($arr, true);
+		private function doPrettyPrint($someVar, $comment = '') {
+			$uniqueId = $this->genUniqueStr();
+			
+			$r = $this->doRecursion($someVar, true, 0 , $uniqueId);
 
 			$o = <<<EOF
-<div style="border: 1px solid #d3d3d3; border-radius: 4px; padding: 10px; font-size: 14px; font-family: 'Courier New', Arial; background-color: rgb(255, 255, 255);">{$r}</div>
+<script>
+	function {$uniqueId}_showCollapsedKeys(lev) {
+		var c = '{$this->libPrefix}le'+lev;
+		var elems = document.getElementById('{$uniqueId}_div').getElementsByClassName(c);
+		for (var i = 0; i != elems.length; ++i) {
+			elems[i].style.display = "inline";
+		}
+	}
+</script>
+<div id="{$uniqueId}_div">
+	<div>{$comment}</div>
+	<div style="border: 1px solid #d3d3d3; border-radius: 4px; padding: 10px; font-size: 14px; font-family: 'Courier New', Arial; background-color: #eee;">{$r}</div>
+</div>
 EOF;
 
 			return $o;
 		}
 
 
-		public static function do_($v) {
+		// echoes
+		public static function dump() {
+			$args       = func_get_args();
+			$argsCount  = count($args);
+			
 			$tmp = new self();
-			return $tmp->doPrettyPrint($v);
+
+			foreach ($args as $i => $v) {
+				echo $tmp->doPrettyPrint($v);
+				
+				if ($i+1 < $argsCount)
+					echo $tmp->manySeparator;
+			}
+			
+		}
+		
+		// returns
+		public static function one($v, $comment = '') {
+			$tmp = new self();
+			return $tmp->doPrettyPrint($v, $comment);
 		}
 	}
--- demo.php
+++ demo.php
@@ -11,16 +11,13 @@
 	
 		$testsArr = array(
 			array(
-				"1"				=> "PHP code tester Sandbox Online",
 				"justTrue"      => true,
 				"foo"			=> "bar",
-				5,
 				"nestedArray"   => array(
-					'key1' => 1, 1.59, 2, 3, "132456", "more" => array(7, 8, 9, "1010101", [11, 222, 3333, 2, 3, 78, 5, 55.5, 6, 7])
+					'key1' => 1, 1.59, 2, 3, "132456", "more" => array(7, 8, 9, "1010101", [11, 222, 3333, 55.5, 3, 78, 5, 2, 6, 7])
 				),
 				"mongoId"       => new DateTime(),
 				5				=> null,
-				"case"			=> "Random Stuff",
 				"f"             => false,
 				"PHP Version"   => phpversion(),
 				"long string"   => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
@@ -30,19 +27,28 @@
 			"some string",
 			true, false,
 			null,
-			[1, 2, 3]
+			[1, 2, 3],
+			[]
 		);
-	
+		
+		echo "<h2>Dump example, code:<br>pp::dump(123, \"string\", [1, 2, 5 => [\"q\", \"w\", \"r\", \"t\", \"y\"]]);</h2>";
+		$arr = [1, 2, 5 => ["q", "w", "r", "t", "y"]];
+		pp::dump(123, "string", $arr);
+
+		echo "<br>";
+		echo "<br>";
+
+		echo "<h2>Other examples, code:<br>PP::one(\$demoVar, \$comment)</h2>";
+			
 		foreach ($testsArr as $i => $t) {
 			$testNumber = $i+1;
 	
 			echo "<br>";
 			$comment = "Demo #".$testNumber."<br>";
-			echo $comment;
-			echo PP::do_($t);
-			echo "<br><hr>";
+			echo PP::one($t, $comment);
+			echo "<br><br>";
 		}
-	
+
 	?>
 </body>
 </html>
